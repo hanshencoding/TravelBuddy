fd4408d2849d28bef7a38c88e18bbe8cf7e3c671
Hanshen Leonardo <hanshenleonardo@gmail.com>
2026-05-20 09:22:42 +0700

    minta profile

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service.js';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma : PrismaService){}
+    async getProfile(userId: string){
+        const user = await this.prisma.user.findUnique({
+            where: { id: userId },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                avatar: true,
+            },
+        })
+    }
+}
